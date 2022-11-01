985ff75c77857fd68d317d730280eddbf0d392ff
atacan <atacan@tutanota.com>
2022-11-01 22:12:35 +0100

    support for control flows

--- Sources/SwiftCss/Builders/PropertyBuilder.swift
+++ Sources/SwiftCss/Builders/PropertyBuilder.swift
@@ -14,4 +14,32 @@ public enum PropertyBuilder {
     public static func buildBlock(_ components: [Property]) -> [Property] {
         components
     }
+    
+    public static func buildBlock(_ components: [Property]...) -> [Property] {
+        components.flatMap { $0 }
+    }
+
+    public static func buildEither(first component: [Property]) -> [Property] {
+        component
+    }
+
+    public static func buildEither(second component: [Property]) -> [Property] {
+        component
+    }
+
+    public static func buildOptional(_ component: [Property]?) -> [Property] {
+        component ?? []
+    }
+
+    public static func buildExpression(_ expression: Property) -> [Property] {
+        [expression]
+    }
+
+    public static func buildExpression(_ expression: [Property]) -> [Property] {
+        expression
+    }
+
+    public static func buildArray(_ components: [[Property]]) -> [Property] {
+        components.flatMap { $0 }
+    }
 }
